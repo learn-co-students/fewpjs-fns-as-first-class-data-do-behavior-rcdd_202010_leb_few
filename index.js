d365e8c768b8ef389fc6c523f966ab30a12c3b46
Jana Khanji <jana.khanji@ieee.org>
2020-11-16 11:45:11 +0000

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,25 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time){
+  let hour=time.split(":")[0];
+  if(hour<=12){
+    return "Good Morning";
+  }
+  else if(hour>17 && hour<=24){
+    return "Good Evening";
+  }
+  else if(hour>24){
+   return "Error, Enter a valid hour";
+  }
+  else {
+    return "Good Afternoon";
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(message){
+  const greeting = document.getElementById('greeting');
+  greeting.innerText=message;
+}
+
+
