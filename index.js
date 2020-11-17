903f91f39851bc16a3eaa6f2c23d700811e216cc
Jolie Hobloss <jolieshobloss@gmail.com>
2020-11-17 15:03:01 +0000

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  const timeArray = time.split(':');
+  let hours=parseInt(timeArray[0]);
+  let result='';
+  if(hours<12){
+    result="Good Morning";
+  }else if(hours<12 ||  hours<17){
+    result="Good Afternoon";
+  }else if(hours>17){
+    result="Good Evening";
+  }
+  return result;
+}
+ 
+
 /* Write your implementation of displayMessage() */
+function displayMessage(msg){
+  document.getElementById('greeting').innerText= msg; 
+}
